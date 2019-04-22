a4abfacb125fbdd910eb42f3e23d224660298c27
tutunak <dk@tutunak.com>
2019-04-22 12:00:08 +0300

    add class for testing find_articles

--- tests/test_aee_parser.py
+++ tests/test_aee_parser.py
@@ -1,14 +1,11 @@
 import unittest
-from aee_parser import get_url
+from aee_parser import get_url, find_articles
 
 
 class TestGetUrl(unittest.TestCase):
     SITE = "https://www.allearsenglish.com/episodes/"
     TIMEOUT = "http://example.com:8000"
     STATUS_CODE = "http://ya.ru/33"
-
-    def setUp(self) -> None:
-        pass
 
     def test_timeout(self):
         result = get_url(self.TIMEOUT)
@@ -22,6 +19,28 @@ class TestGetUrl(unittest.TestCase):
         result = get_url(self.SITE)
         self.assertEqual(0, result['status'])
         self.assertIsInstance(result['data'], str)
+
+
+class TestFindArticles(unittest.TestCase):
+    with open("pages/page-2.html") as f:
+        page = f.read()
+
+    def test_valid_data(self):
+        result = {
+            "status": 0,
+            "data": self.page
+        }
+        articles = find_articles(result)
+        self.assertIsInstance(articles, list)
+        self.assertGreater(len(articles), 3)
+
+    def test_not_valid_page_result(self):
+        result = {
+            "status": 1,
+            "data": "data"
+        }
+        articles = find_articles(result)
+        self.assertIs(articles, None)
 
 
 if __name__ == '__main__':
