77260b79e9d5aade7e13016e906d6703cf85f43f
tutunak <dk@tutunak.com>
2019-04-22 11:33:53 +0300

    add test for valid data

--- tests/test_aee_parser.py
+++ tests/test_aee_parser.py
@@ -3,16 +3,25 @@ from aee_parser import get_url
 
 
 class TestGetUrl(unittest.TestCase):
+    SITE = "https://www.allearsenglish.com/episodes/"
+    TIMEOUT = "http://example.com:8000"
+    STATUS_CODE = "http://ya.ru/33"
+
     def setUp(self) -> None:
         pass
 
     def test_timeout(self):
-        result = get_url("http://example.com:8000")
+        result = get_url(self.TIMEOUT)
         self.assertDictEqual(result, {"status": 1, "data": "ConnectionError"})
 
     def test_status_code(self):
-        result = get_url("http://ya.ru/33")
+        result = get_url(self.STATUS_CODE)
         self.assertDictEqual(result, {"status": 1, "data": 'Http error: 404'})
+
+    def test_page_is_valid(self):
+        result = get_url(self.SITE)
+        self.assertEqual(0, result['status'])
+        self.assertIsInstance(result['data'], str)
 
 
 if __name__ == '__main__':
