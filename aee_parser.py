ffdef7fb458f2c6bf33a4f8d87ab79fad60a6c28
tutunak <dk@tutunak.com>
2019-04-24 12:36:14 +0300

    now aee is class

--- aee_parser.py
+++ aee_parser.py
@@ -7,6 +7,45 @@ import requests
 SITE = "https://www.allearsenglish.com/episodes/"
 
 
+class AEE:
+    """
+    AEE podcast site class
+    """
+    def __init__(self, url):
+        self.url = url
+        self.page = None
+        self.articles = None
+        self.__soup = None
+
+    def __iter__(self):
+        return self
+
+    def __get_articles(self):
+        if self.page:
+            self.__soup = Bs(self.page, 'lxml')
+            self.articles = self.__soup.find_all(
+                'article', attrs={'class': 'post'})
+
+    def __get_url(self):
+        resp = requests.get(self.url)
+        resp.raise_for_status()
+        self.page = resp.text
+
+    def __find_next(self):
+        numeric_pagination = self.__soup.find(
+            'div', attrs={'class': 'numeric-pagination'}
+        )
+        return numeric_pagination.find('a', string='>').href
+
+    def __next__(self):
+        if not self.page:
+            self.__get_url()
+        next_page = self.__find_next()
+        if next_page:
+            return AEE(next_page)
+        raise StopIteration
+
+
 def get_url(url):
     """
     Return page content
@@ -43,8 +82,12 @@ def main():
     Main function
     :return: None
     """
-    page_result = get_url(SITE)
-    find_articles(page_result)
+    # page_result = get_url(SITE)
+    # for article in find_articles(page_result):
+    #     post_link = get_post_link(article)
+    #     post_data = get_url(post_link)
+    #
+    # return post_data
 
 
 if __name__ == '__main__':
